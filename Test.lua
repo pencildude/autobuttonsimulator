2285f7477d863ffa3d9c460cbcfe84c4ad29c4f6
pencildude <92550739+pencildude@users.noreply.github.com>
2021-10-14 17:37:43 -0500

    added thingies
    why are these gas prices so high wawa

--- Test.lua
+++ Test.lua
@@ -21,59 +21,77 @@ local priorityList = {
     ["Event Power"] = 4,
     ["Leaf"] = 5,
 }
+local isactive = false
+local window = library.CreateLib("Button Pressor", "DarkTheme")
+local tab = window:NewTab("Main")
+local section = tab:NewSection("Commands")
+local togglebutton
+local speed = 100
+local runNumber = 0
+togglebutton = section:NewButton("Toggle bot (inactive)", "les go", function()   
+    isactive = not isactive
+    local newtext = isactive and " (active)" or " (inactive)"
+    togglebutton:UpdateButton("Toggle bot"..newtext) 
+    print(newtext)
+end)
+section:NewSlider("Bot speed", "higher is better", 100, 0, function(s)
+    speed = s
+end)
 
-local Window = Library.CreateLib("TITLE", "DarkTheme")
-            
+
+
 local connect
 connect = game:GetService("RunService").Stepped:Connect(function()
-    --temporary
-    if game:GetService("UserInputService"):IsKeyDown(Enum.KeyCode.E) then
-        connect:Disconnect()
-    end
-    
-    for _,button in ipairs(buttons) do
-        local statNeeded = button:GetAttribute("StatNeeded")
-        if statNeeded then
-            local data = playerData:FindFirstChild(statNeeded)
-            local statAmount = button:GetAttribute("StatAmount")
-            
-            if eternity.meeq(data.Value,statAmount) then
-                if highestAvailableStats[statNeeded] then
-                    if highestAvailableStats[statNeeded]:GetAttribute("StatAmount") < statAmount then
+    if isactive and runNumber < speed then
+        
+        runNumber = runNumber + 1 
+        
+        for _,button in ipairs(buttons) do
+            local statNeeded = button:GetAttribute("StatNeeded")
+            if statNeeded then
+                local data = playerData:FindFirstChild(statNeeded)
+                local statAmount = button:GetAttribute("StatAmount")
+                
+                if eternity.meeq(data.Value,statAmount) then
+                    if highestAvailableStats[statNeeded] then
+                        if highestAvailableStats[statNeeded]:GetAttribute("StatAmount") < statAmount then
+                            highestAvailableStats[statNeeded] = button
+                        end
+                    else
                         highestAvailableStats[statNeeded] = button
                     end
-                else
-                    highestAvailableStats[statNeeded] = button
                 end
             end
         end
-    end
-    
-    --local availableStatsArray = {}
-    local highestButton
-    local highestValue = 0
-    
-    for statNeeded,button in pairs(highestAvailableStats) do
-        --print(statNeeded,priorityList[statNeeded])
-        if priorityList[statNeeded] > highestValue then
-            highestButton = button
-            highestValue = priorityList[statNeeded]
+        
+        --local availableStatsArray = {}
+        local highestButton
+        local highestValue = 0
+        
+        for statNeeded,button in pairs(highestAvailableStats) do
+            --print(statNeeded,priorityList[statNeeded])
+            if priorityList[statNeeded] > highestValue then
+                highestButton = button
+                highestValue = priorityList[statNeeded]
+            end
+            --table.insert(availableStatsArray,{statNeeded,button})
         end
-        --table.insert(availableStatsArray,{statNeeded,button})
-    end
-    
-    --[[table.sort(availableStatsArray,function(a,b)
-        return priorityList[a[1] > priorityList[b[1]
-    end)]]
-    
-    if highestButton then
-        character:PivotTo(highestButton.PrimaryPart.CFrame + Vector3.new(0,3.5,0))
-        --task.wait(0.5)
-    end
-    
-    --[[for _,stats in ipairs(availableStatsArray) do
-        character:PivotTo(stats[2].PrimaryPart.CFrame + Vector3.new(0,3.5,0))
-        task.wait(0.5)
-    end]]
-end)
+        
+        --[[table.sort(availableStatsArray,function(a,b)
+            return priorityList[a[1] > priorityList[b[1]
+        end)]]
+        
+        if highestButton then
+            character:PivotTo(highestButton.PrimaryPart.CFrame + Vector3.new(0,3.5,0))
+            --task.wait(0.5)
+        end
+        
+        --[[for _,stats in ipairs(availableStatsArray) do
+            character:PivotTo(stats[2].PrimaryPart.CFrame + Vector3.new(0,3.5,0))
+            task.wait(0.5)
+        end]]
+        else
+            runNumber = 0
+        end
+    end)
 end))
